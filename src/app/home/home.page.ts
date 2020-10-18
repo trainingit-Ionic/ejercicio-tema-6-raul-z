4b250a8fe75511acb477c2752d7d91b4014955a2
raúl <raul.zori@ara-tech.es>
2020-10-18 23:16:06 +0200

    Ejercicio 6

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as Users from '../data/users';
+import { CallNumber } from '@ionic-native/call-number/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public usersData : any; 
+  public selected : boolean;
+  public selectedUser: any; 
 
+  constructor(private callNumber: CallNumber) { }
+
+  
+  
+  
+
+  ngOnInit(): void {
+    this.selected = false;    
+    this.usersData = Users.USERS;
+  }
+
+  showUserDetails(user): void {
+    this.selectedUser = user;
+    this.selected = true;
+  }
+
+  hideUserDetails(): void {
+    this.selected = false;
+  }
+
+  openDialer(): void {
+    this.callNumber.callNumber(this.selectedUser.phone, true)
+    .then(res => console.log('Launched dialer!', res))
+    .catch(err => console.log('Error launching dialer', err));
+  } 
 }
+
